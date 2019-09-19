52937d3d0380cefbcd59bbeacf1068c5012305c5
github username <github email address>
2019-09-19 16:31:47 -0400

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,61 +1,43 @@
-class CashRegister
-    
-    attr_accessor :items, :discount, :total, :last_transaction
+class CashRegister
 
-    def initialize(discount=0)
-      @total = 0
-      @discount = discount
-      @items = []
-    end
-
-    # def add_item(title, price, quantity=1)
-    #     @title = title
-    #     @price = price
-    #     @quantity = quantity
-    #     @total += price
-    #     item << title
-    # end
-
-    def add_item(title, amount, quantity=1)
-        self.total += amount * quantity
-        quantity.times do
-          items << title
-        end
-        self.last_transaction = amount * quantity
-    end
-    
-    def apply_discount
-        if discount != 0
-          self.total = (total * ((100.0 - discount.to_f)/100)).to_i
-          "After the discount, the total comes to $#{self.total}."
-        else
-          "There is no discount to apply."
-        end
-    end
-    
-    def void_last_transaction
-        self.total = self.total - self.last_transaction
-    end
-
-    # def apply_discount
-    #     if @discount
-    #     @total *= ((100-@discount)/100)
-    #     puts "success message"
-    #     else puts "there is no discount"
-    #     end
-    # end
-
-    # def item
-    #     return item
-    # end
-
-    # def void_last_transaction
-    #     @total - self.price
-    #     item.slice[-1]
-    #     if item.length == 0
-    #         @total = 0
-    #     end
-    # end
-
-
-end
+      attr_accessor :total, :discount, :title, :item, :last_transaction
+  
+  
+      def initialize(discount=0)
+          @total = 0
+          @item = []
+          @discount = discount
+      end
+    
+      # default quantity should be 1!
+      # items with same name still should be added on the item list, so do "quantity.times"
+      # need to call last_transaction here because only add_item can track what was the last
+      # item added on the list
+      def add_item(title, price, quantity=1)
+          self.total += price * quantity
+          quantity.times do
+          @item << title
+          end
+          self.last_transaction = price * quantity
+      end
+  
+      def apply_discount
+          if @discount > 0
+             self.total = (self.total * (1-discount.to_f/100)).to_i
+             return "After the discount, the total comes to $#{self.total}."
+          else "There is no discount to apply."
+          end
+      end
+  
+      def items
+          @item
+      end
+  
+      def void_last_transaction
+          self.total = self.total - self.last_transaction
+          if self.items.length == 0
+           self.total = 0
+          end
+      end
+  
+  end
